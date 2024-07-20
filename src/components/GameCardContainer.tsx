8243aa977e23befd5388b4d099380810a283e730
Yu Lai <yulai.linda@gmail.com>
2024-07-20 11:32:37 -0400

    Display the genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface GameCardContainerProps {
 
 const GameCardContainer = ({ children }: GameCardContainerProps) => {
   return (
-    <Box width="250px" borderRadius={20} overflow="hidden">
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
