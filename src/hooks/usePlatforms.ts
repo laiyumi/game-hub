d4101e6938394a18d0abc8cf2e665f70ff460d22
Yu Lai <yulai.linda@gmail.com>
2024-07-27 15:36:28 -0400

    refactoring-remove duplicate interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
-import { Platform } from "./useGames";
 import { FetchResponse } from "../services/api-client";
 
-// const usePlatforms = ()=> useData<Platform>("/platforms/lists/parents");
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 
 const usePlatforms = () =>
   useQuery({
